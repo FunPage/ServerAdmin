a1a92fcb4828220c36d959def1c2fdd2f349affb
taggo <taggo@taggo>
2013-12-12 00:02:10 +0530

    table display successfull using colname......

--- ServerAdmin/src/serveradmin/searchdb.java
+++ ServerAdmin/src/serveradmin/searchdb.java
@@ -3,16 +3,26 @@
  * and open the template in the editor.
  */
 package serveradmin;
+import com.mysql.jdbc.*;
 import Framework.*;
 import java.sql.SQLException;
+import java.sql.ResultSet;
 /**
  *
  * @author taggo
  */
 public class searchdb {
     searchdb() throws SQLException, ClassNotFoundException
-    {
-        
+    {try{
+        basicop b=new basicop();
+        b.setUrl("jdbc:mysql://localhost:3306/");
+        ResultSet rs=b.Listenquery("show databases");
+        String[] sample=null;
+        String[][] hello={{"2"}};
+        sample=b.getColumnNames(rs);
+        }
+    catch(Exception e){System.out.println("exception");
+    e.printStackTrace();}
     }
         
 }
